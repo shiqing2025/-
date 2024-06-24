d8535599267c7a4889881eafc5e63f97b24184f6
shiqing2025 <shiqing2025@foxmail.com>
2024-06-24 22:29:47 +0800

    深拷贝

--- deepCopy.js
+++ deepCopy.js
@@ -1,13 +1,14 @@
 // 1. JSON.parse(JSON.stringify());
 const fn1 = (obj) => {
   return JSON.parse(JSON.stringify(obj));
-}
+};
 
-// 2. 创建一个新的对象，遍历需要克隆的对象，将需要克隆对象的属性依次添加到新对象上
+// 2. Create a new object, traverse the object to be cloned,
+// and add the properties of the object to be cloned to the new object one by one
 const fn2 = (obj) => {
-  if (typeof obj === 'object' && obj !== null) {  // 注意值为 null
-
-    const newObj = Array.isArray(obj) ? [] : {}; // 注意值为 数组
+  if (typeof obj === "object" && obj !== null) {
+    // Determine whether the object is an object or an array
+    const newObj = Array.isArray(obj) ? [] : {};
     for (const key in obj) {
       newObj[key] = fn2(obj[key]);
     }
@@ -15,12 +16,13 @@ const fn2 = (obj) => {
   } else {
     return obj;
   }
-}
+};
 
-// 3. 处理循环引用 额外开辟一个存储空间，来存储当前对象和拷贝对象的对应关系
+// 3. Handle circular references by allocating an additional storage space
+// to store the corresponding relationship between the current object and the copied object
 const fn3 = (obj, map = new WeakMap()) => {
-  if (typeof obj === 'object' && obj !== null) {
-    if (map.get(obj)) return map.get(obj);  // 注意循环引用
+  if (typeof obj === "object" && obj !== null) {
+    if (map.get(obj)) return map.get(obj); // Be careful of circular references
 
     const newObj = Array.isArray(obj) ? [] : {};
     map.set(obj, newObj);
@@ -32,31 +34,39 @@ const fn3 = (obj, map = new WeakMap()) => {
   } else {
     return obj;
   }
-}
+};
 
+// Util function 1 - Get data type
+const getType = (target) => {
+  const typeStr = Object.prototype.toString.call(target); // 注意 Object.prototype.toString.call() 的用法
+  const spaceIdx = typeStr.indexOf(" ");
+  const type = typeStr.splice(spaceIdx, -1);
+  return type;
+};
 
+// Util function 2 - Get the initialization data of different types of data, such as [] and {}
+const getInitData = (target) => {
+  const Ctor = target.constructor; // Every object has a constructor property that points to the constructor function that created the object
+  return new Ctor(); // new Array() or new Object() etc.
+};
 
-
-const obj1 = {   // 原数据，包含字符串、对象、函数、数组等不同的类型
+const obj1 = {
+  // original data, including strings, objects, functions, arrays and other different types etc.
   name: "test",
   city: null,
   main: {
     a: 1,
-    b: 2
+    b: 2,
   },
-  fn: function () {
-
-  },
+  fn: function () {},
   friends: [1, 2, 3, [22, 33]],
-}
-obj1.child = obj1; // 对象存在循环引用的情况，即对象的属性间接或直接的引用了自身的情况
+};
+obj1.child = obj1; //  object has circular references, meaning the properties of the object indirectly or directly refer to itself.
 
 const obj2 = fn3(obj1);
 
-obj1.name = 'qq';
+obj1.name = "qq";
 obj2.main.a = 2222;
 
-
-console.log('obj1:', obj1);
-console.log('obj2:', obj2);
-
+console.log("obj1:", obj1);
+console.log("obj2:", obj2);
